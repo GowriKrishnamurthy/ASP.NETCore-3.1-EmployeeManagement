55b9e51148d4f6a93d68377706bb906e4002e35d
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-06-13 15:37:34 +1000

    Controller with DI

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController
+    {
+        private readonly IEmployeeService employeeRepository;
+        
+        // Constructor Injection
+        public HomeController(IEmployeeService employeeRepository)
+        { 
+            this.employeeRepository = employeeRepository;
+        }
+
+        public string Index()
+        {
+            return employeeRepository.GetEmployee(1).Name;
+        }
+    }
+}
